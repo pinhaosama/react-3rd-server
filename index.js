1d7f5db723e72149a1372e48675a26302a71a604
pinhaosama <gabrielpinhao1@gmail.com>
2023-10-17 19:14:48 -0700

    update GET request test

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const port = 3000; // the port the server will be listening on
 const textBodyParser = bodyParser.text({ limit: '20mb', defaultCharset: 'utf-8'});
 
 app.use(cors({
-    origin: 'http://localhost:5173' 
+    origin: 'http://localhost:5000' 
 }));
 
 app.options('/racedata', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Headers', 'task'); // Allow the 'task 'header
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, task"); // Allow the 'task 'header
     res.header('Access-Control-Allow-Methods', 'GET'); // Allow the GET method
     // res.header('Access-Control-Allow-Methods', 'POST'); // Allow the POST method
     // res.header('Access-Control-Allow-Methods', 'PUT'); // Allow the POST method
@@ -23,14 +23,16 @@ app.options('/racedata', (req, res) => {
 
 app.get('/racedata', async (req, res) => {
     console.log('receiveing Get request at /racedata with task: ' + req.headers['task']);
-    try{
-        const fileData = await fs.promises.readFile('data/products.json');
-        const racers = JSON.parse(fileData);
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json(racers);
-    } catch {
-        console.error('Error loading todos:', error);
-        res.status(500).json({ error: 'Error loading todos' });
+    if(req.headers['task'] === 'getStuff'){
+        try{
+            const fileData = await fs.promises.readFile('data/points.json');
+            const racers = JSON.parse(fileData);
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json(racers);
+        } catch {
+            console.error('Error loading todos:', error);
+            res.status(500).json({ error: 'Error loading todos' });
+        }    
     }
 })
 
